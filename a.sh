4c03adabb5179bf7c9cc135bf269061c61b26dc1
edwardsatoshi <43865519+edwardsatoshi@users.noreply.github.com>
2018-10-04 20:22:02 -0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -12,13 +12,12 @@ while true; do
 	  echo "Mismatch. try again"
 done
 
-
 setup(){
   # uefi
-  [ -d "/sys/firmware/efi/efivar" ] || { echo "Fail: no uefi."; exit 1}
+  [ -d "/sys/firmware/efi/efivar" ] || ( echo "Fail: no uefi."; exit 1)
 
   #internet
-  ping -q -c 1 -W 1 https://www.archlinux.org >/dev/null || {echo -e "Fail: no internet. tip: \e[32mGreen wifi-menu \e[39mDefault command connects to wifi\n "; exit 1}
+  ping -q -c 1 -W 1 https://www.archlinux.org >/dev/null ||(echo -e "Fail: no internet. tip: \e[32mGreen wifi-menu \e[39mDefault command connects to wifi\n "; exit 1)
 
   timedatectl set-ntp true
 
@@ -38,7 +37,7 @@ setup(){
   genfstab -U /mnt >> /mnt/etc/fstab
   arch-chroot /mnt
   printf "$user\n$host\n$pw\n$pw\n" arch-chroot sh $0 chroot
-
+}
 
 configure(){
 
